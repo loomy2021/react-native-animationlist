9ca4169e0edcb167e55efab78c1b194c44691389
loomy2021 <stev@loomcomputer.com>
2024-07-18 15:09:14 +0200

    expo empty project installed unversioned files added

---
+++ App.js
@@ -0,0 +1,17 @@
+import {s} from "./App.style";
+import {  Text, View } from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+      <SafeAreaProvider>
+        <SafeAreaView >
+          <View style={s.container} >
+            <Text style={s.txt}>123</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+
+
+  );
+}
